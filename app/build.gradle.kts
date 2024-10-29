e4fb1986ee42371a6b2e3643b4ea8b183ce3e9d7
Misael Villar Julian <devvillar@gmail.com>
2024-10-29 14:32:35 -0600

    Generar .zip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
+    //alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
     id("maven-publish")
 }
 
-
 android {
-    namespace = "com.devvillar.demolibrary"
+    namespace = "com.devvillar.dateutils"
 
     publishing {
         singleVariant("release") {
@@ -23,8 +23,27 @@ android {
         //versionCode = 1
         //versionName = "1.0"
 
+        aarMetadata {
+            minCompileSdk = 27
+        }
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+//    productFlavors {
+//        register("foo") {
+//
+//            aarMetadata {
+//                minCompileSdk = 27
+//            }
+//        }
+//        register("bar") {
+//            aarMetadata {
+//                minCompileSdk = 27
+//            }
+//        }
+//
+//    }
 
     buildTypes {
         release {
@@ -44,53 +63,56 @@ android {
     }
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+
+            groupId = "io.github.devvillar"
+            artifactId = "dateutils"
+            version = "1.0.0"
+
+            afterEvaluate {
                 from(components["release"])
-                groupId = "io.github.devvillar"
-                artifactId = "demolibrary"
-                version = "1.0.0"
+            }
 
-                pom {
-                    packaging = "aar"
-                    name = "demolibrary"
-                    description = "Demolibrary: Convertir fecha a un formato amigable"
-                    url = "URL del proyecto"
+            pom {
+                packaging = "aar"
+                name = "dateutils"
+                description = "DateUtils: Convertir fecha a un formato amigable"
+                url = "https://github.com/devVillar/demoLibrary.git"
 
-                    licenses {
-                        license {
-                            name = "The Apache License, Version 2.0"
-                            url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                        }
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
                     }
+                }
 
-                    developers {
-                        developer {
-                            id = "IDDevVillar"
-                            name = "Misael Villar"
-                            email = "devvillar@gmail.com"
-                        }
+                developers {
+                    developer {
+                        id = "IDDevVillar"
+                        name = "Misael Villar"
+                        email = "devvillar@gmail.com"
                     }
+                }
 
-                    scm {
-                        connection = "scm:git:git://github.com/usuario/proyecto.git"
-                        developerConnection = "scm:git:ssh://github.com:usuario/proyecto.git"
-                        url = "https://github.com/usuario/proyecto"
-                    }
+                scm {
+                    connection = "scm:git:git://github.com/devVillar/demoLibrary.git"
+                    developerConnection = "scm:git:ssh://github.com/devVillar/demoLibrary.git"
+                    url = "https://github.com/devVillar/demoLibrary"
                 }
             }
         }
+    }
 
-        repositories {
-            maven {
-                name = "sonatype"
-                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = (project.findProperty("ossrhUsername") ?: "").toString()
-                    password = (project.findProperty("ossrhPassword") ?: "").toString()
-                }
+    repositories {
+        maven {
+            name = "sonatype"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = (project.findProperty("ossrhUsername") ?: "").toString()
+                password = (project.findProperty("ossrhPassword") ?: "").toString()
             }
         }
     }
